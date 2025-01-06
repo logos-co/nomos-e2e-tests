6121666eefb1de475533205334149d3c2723bb1c
Roman <zajic@zajic.net>
2025-01-06 15:41:10 +0800

    fix: node vars with config location

--- src/node/node_vars.py
+++ src/node/node_vars.py
@@ -1,15 +1,15 @@
 nomos_nodes = {
     "nomos": {
         "image": "nomos/nomos:latest",
-        "volumes": ["./testnet:/etc/nomos", "./tests/kzgrs/kzgrs_test_params:/kzgrs_test_params:z"],
+        "volumes": ["./cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params.bin:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_node.sh",
     },
     "nomos_executor": {
         "image": "nomos/nomos:latest",
-        "volumes": ["./testnet:/etc/nomos", "./tests/kzgrs/kzgrs_test_params:/kzgrs_test_params:z"],
+        "volumes": ["./cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params.bin:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_executor.sh",
     },
-    "cfgsync": {"image": "nomos/nomos:latest", "volumes": ["./testnet:/etc/nomos"], "ports": "", "entrypoint": "/etc/nomos/scripts/run_cfgsync.sh"},
+    "cfgsync": {"image": "nomos/nomos:latest", "volumes": ["./cl_config:/etc/nomos"], "ports": "", "entrypoint": "/etc/nomos/scripts/run_cfgsync.sh"},
 }
