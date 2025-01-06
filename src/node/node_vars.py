fd6b700add3f52c067f5e0dbb35a329a9ee288eb
Roman <zajic@zajic.net>
2025-01-06 18:34:00 +0800

    fix: use prebuild image

--- src/node/node_vars.py
+++ src/node/node_vars.py
@@ -1,15 +1,20 @@
 nomos_nodes = {
     "nomos": {
-        "image": "nomos/nomos:latest",
+        "image": "ghcr.io/logos-co/nomos-node:testnet",
         "volumes": ["./cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params.bin:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_node.sh",
     },
     "nomos_executor": {
-        "image": "nomos/nomos:latest",
+        "image": "ghcr.io/logos-co/nomos-node:testnet",
         "volumes": ["./cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params.bin:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_executor.sh",
     },
-    "cfgsync": {"image": "nomos/nomos:latest", "volumes": ["./cl_config:/etc/nomos"], "ports": "", "entrypoint": "/etc/nomos/scripts/run_cfgsync.sh"},
+    "cfgsync": {
+        "image": "ghcr.io/logos-co/nomos-node:testnet",
+        "volumes": ["./cl_config:/etc/nomos"],
+        "ports": "",
+        "entrypoint": "/etc/nomos/scripts/run_cfgsync.sh",
+    },
 }
