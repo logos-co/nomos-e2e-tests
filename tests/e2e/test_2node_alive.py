084c794ab69a99b7401e71fe2bbc6f4e9805e4a3
Roman <zajic@zajic.net>
2025-01-07 13:26:37 +0800

    fix: conflict in container name

--- tests/e2e/test_2node_alive.py
+++ tests/e2e/test_2node_alive.py
@@ -8,7 +8,7 @@ class Test2NodeClAlive:
 
         self.node1 = NomosNode(CFGSYNC, "cfgsync")
         self.node2 = NomosNode(NOMOS, "nomos_node_0")
-        self.node3 = NomosNode(NOMOS_EXECUTOR, "nomos_node_0")
+        self.node3 = NomosNode(NOMOS_EXECUTOR, "nomos_node_1")
 
         self.node1.start()
         self.node2.start()
