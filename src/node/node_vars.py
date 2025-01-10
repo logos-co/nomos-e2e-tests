8a846fd4502729f4ce13f1545909c43296879ea0
Roman <zajic@zajic.net>
2025-01-10 14:32:59 +0800

    test: change to test image name

--- src/node/node_vars.py
+++ src/node/node_vars.py
@@ -1,18 +1,18 @@
 nomos_nodes = {
     "nomos": {
-        "image": "ghcr.io/logos-co/nomos-node:testnet",
+        "image": "nomos:latest",
         "volumes": ["cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params.bin:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_node.sh",
     },
     "nomos_executor": {
-        "image": "ghcr.io/logos-co/nomos-node:testnet",
+        "image": "nomos:latest",
         "volumes": ["cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params.bin:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_executor.sh",
     },
     "cfgsync": {
-        "image": "ghcr.io/logos-co/nomos-node:testnet",
+        "image": "nomos:latest",
         "volumes": ["cl_config:/etc/nomos"],
         "ports": "",
         "entrypoint": "/etc/nomos/scripts/run_cfgsync.sh",
