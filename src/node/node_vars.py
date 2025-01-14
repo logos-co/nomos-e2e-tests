88ddf782528202768931d742155daf3777d2e6a9
Roman <zajic@zajic.net>
2025-01-14 13:39:12 +0800

    fix: cluster config dir name

--- src/node/node_vars.py
+++ src/node/node_vars.py
@@ -1,19 +1,19 @@
 nomos_nodes = {
     "nomos": {
         "image": "nomos:latest",
-        "volumes": ["cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params:/kzgrs_test_params:z"],
+        "volumes": ["cluster_config:/etc/nomos", "./kzgrs/kzgrs_test_params:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_node.sh",
     },
     "nomos_executor": {
         "image": "nomos:latest",
-        "volumes": ["cl_config:/etc/nomos", "./kzgrs/kzgrs_test_params:/kzgrs_test_params:z"],
+        "volumes": ["cluster_config:/etc/nomos", "./kzgrs/kzgrs_test_params:/kzgrs_test_params:z"],
         "ports": ["3000/udp", "18080/tcp"],
         "entrypoint": "/etc/nomos/scripts/run_nomos_executor.sh",
     },
     "cfgsync": {
         "image": "nomos:latest",
-        "volumes": ["cl_config:/etc/nomos"],
+        "volumes": ["cluster_config:/etc/nomos"],
         "ports": "",
         "entrypoint": "/etc/nomos/scripts/run_cfgsync.sh",
     },
