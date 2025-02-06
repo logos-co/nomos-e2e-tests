3ee98528c2d1f5fa0d2de9f4c886c2e4fc34cff8
Roman <zajic@zajic.net>
2025-02-06 16:09:30 +0800

    test: reconstruct command

--- tests/data_integrity/test_data_integrity.py
+++ tests/data_integrity/test_data_integrity.py
@@ -1,7 +1,15 @@
+import json
+import subprocess
+
 import pytest
 
+from src.env_vars import NOMOS_CLI
+from src.libs.common import delay
+from src.libs.custom_logger import get_custom_logger
 from src.steps.da import StepsDataAvailability
 from src.test_data import DATA_TO_DISPERSE
+
+logger = get_custom_logger(__name__)
 
 
 class TestDataIntegrity(StepsDataAvailability):
@@ -24,5 +32,20 @@ class TestDataIntegrity(StepsDataAvailability):
     @pytest.mark.usefixtures("setup_2_node_cluster")
     def test_da_sampling_determines_data_presence(self):
         self.disperse_data(DATA_TO_DISPERSE[0], [0] * 31 + [1], [0] * 8)
+        delay(10)
         received_data = self.get_data_range(self.node2, [0] * 31 + [1], [0] * 8, [0] * 7 + [5])
-        assert DATA_TO_DISPERSE[0] == bytes(received_data[0][1]).decode("utf-8")
+        rcv_data_json = json.dumps(received_data)
+        cmd = str(NOMOS_CLI + " reconstruct --app-blobs " + "'" + str(rcv_data_json) + "'")
+
+        logger.debug(f"Command to run {cmd}")
+
+        cmd_type = type(cmd)
+        logger.debug(f"Command type {cmd_type}")
+
+        try:
+            completed_cmd = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+            logger.debug(f"Command finished with output {completed_cmd.stdout}")
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error occurred while running nomos-cli {e.stderr}")
+
+        # assert DATA_TO_DISPERSE[0] == bytes(received_data[0][1]).decode("utf-8")
