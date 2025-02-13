f74002c1086fad93c397e6c4c27bed924ced912e
Roman <zajic@zajic.net>
2025-02-13 17:12:41 +0800

    test: with different data

--- tests/data_integrity/test_data_integrity.py
+++ tests/data_integrity/test_data_integrity.py
@@ -18,7 +18,7 @@ class TestDataIntegrity(StepsDataAvailability):
     @pytest.mark.usefixtures("setup_4_node_cluster")
     def test_da_identify_retrieve_missing_columns(self):
         delay(5)
-        self.disperse_data(DATA_TO_DISPERSE[0], to_app_id(1), to_index(0))
+        self.disperse_data(DATA_TO_DISPERSE[6], to_app_id(1), to_index(0))
         delay(5)
         # Select one target node at random to get blob data for 1/2 columns
         selected_node = self.main_nodes[random.randint(1, 3)]
@@ -32,7 +32,7 @@ class TestDataIntegrity(StepsDataAvailability):
     @pytest.mark.usefixtures("setup_2_node_cluster")
     def test_da_sampling_determines_data_presence(self):
         delay(5)
-        self.disperse_data(DATA_TO_DISPERSE[0], to_app_id(1), to_index(0))
+        self.disperse_data(DATA_TO_DISPERSE[6], to_app_id(1), to_index(0))
         delay(5)
         rcv_data = self.get_data_range(self.node2, to_app_id(1), to_index(0), to_index(5))
         rcv_data_json = json.dumps(rcv_data)
