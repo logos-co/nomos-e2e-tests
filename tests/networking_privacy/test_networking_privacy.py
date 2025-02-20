dd32fe1987c357d46bd7275c92412d9f083b416a
Roman <zajic@zajic.net>
2025-02-21 10:52:10 +1100

    test: consumed bandwidth measurement

--- tests/networking_privacy/test_networking_privacy.py
+++ tests/networking_privacy/test_networking_privacy.py
@@ -1,8 +1,12 @@
 import pytest
+import psutil
 
 from src.libs.common import delay, to_app_id, to_index
+from src.libs.custom_logger import get_custom_logger
 from src.steps.da import StepsDataAvailability
 from src.test_data import DATA_TO_DISPERSE
+
+logger = get_custom_logger(__name__)
 
 
 class TestNetworkingPrivacy(StepsDataAvailability):
@@ -11,4 +15,15 @@ class TestNetworkingPrivacy(StepsDataAvailability):
     @pytest.mark.parametrize("setup_2_node_cluster", [1024], indirect=True)
     def test_consumed_bandwidth_dispersal(self, setup_2_node_cluster):
         delay(5)
+        net_io = psutil.net_io_counters()
+        prev_total = net_io.bytes_sent + net_io.bytes_recv
         self.disperse_data(DATA_TO_DISPERSE[1], to_app_id(1), to_index(0))
+        net_io = psutil.net_io_counters()
+        curr_total = net_io.bytes_sent + net_io.bytes_recv
+
+        logger.debug(f"prev_total: {prev_total}")
+        logger.debug(f"curr_total: {curr_total}")
+
+        consumed = curr_total - prev_total
+
+        logger.debug(f"consumed: {consumed}")
